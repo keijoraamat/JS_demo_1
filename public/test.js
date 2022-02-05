af4b7831699cb1987b86b2f038d21bd3a7318610
Keijo Raamat <raamatkeijo@gmail.com>
2022-02-05 19:16:06 +0200

    Starts calculater buidl

--- public/test.js
+++ public/test.js
@@ -1,24 +1,8 @@
 "use strict";
-var y = {
-    fn: null,
-    saveFn(fn) {
-        this.fn = fn;
-    },
-    execute: function() {
-        this.fn();
-    }
-}
-var x = {
-    foo: function(z){
-        let msg = "inside foo too";
-        let arrowFn = () => {
-            alert(msg);
-        }
 
-        z.saveFn(arrowFn);
-    }
-}
+// finds element by id
+let btn = document.querySelector('#btn-cal');
+let operator =  document.querySelector('#operator');
 
-x.foo(y);
-x = undefined;
-y.execute();
+console.log(btn);
+console.log(operator);
