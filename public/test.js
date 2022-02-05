ca324c262974f43b2d915d68b362906437821035
Keijo Raamat <raamatkeijo@gmail.com>
2022-02-05 17:29:55 +0200

    closure

--- public/test.js
+++ public/test.js
@@ -1,2 +1,24 @@
 "use strict";
+var y = {
+    fn: null,
+    saveFn(fn) {
+        this.fn = fn;
+    },
+    execute: function() {
+        this.fn();
+    }
+}
+var x = {
+    foo: function(z){
+        let msg = "inside foo too";
+        let arrowFn = () => {
+            alert(msg);
+        }
 
+        z.saveFn(arrowFn);
+    }
+}
+
+x.foo(y);
+x = undefined;
+y.execute();
